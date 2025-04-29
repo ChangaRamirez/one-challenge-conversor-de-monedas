5ed6e54b8edc08c5b794758b9e3a082ef61b347d
Changa Ramirez <laloraco@hotmail.com>
2025-04-29 13:01:43 -0600

    Implementa HttpRequest y HttpResponse

--- src/com/alura/conversor/modelos/Conversor.java
+++ src/com/alura/conversor/modelos/Conversor.java
@@ -4,7 +4,10 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Conversor {
     Gson gson = new GsonBuilder()
@@ -63,6 +66,17 @@ public class Conversor {
 
         try {
             HttpClient client = java.net.http.HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            String json = response.body();
+            System.out.println(json);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
